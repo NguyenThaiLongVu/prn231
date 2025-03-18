67e4100938e38d8925c044a178cae8cc96d7e02a
hungle202 <hunglhe161111@fpt.edu.vn>
2025-03-18 17:24:54 +0700

    Hungle202 add requests response model

---
+++ PRN231/PRN231/Requests/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace PRN231.Requests
+{
+    public class Login
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ PRN231/PRN231/Responses/RoomFacilityReponse.cs
@@ -0,0 +1,21 @@
+﻿using PRN231.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PRN231.Responses
+{
+    public class RoomFacilityReponse
+    {
+        public int RoomFacilityID { get; set; }
+
+        public int RoomID { get; set; }
+        public Room? Room { get; set; }
+
+        public int FacilityID { get; set; }
+        public Facility? Facility { get; set; }
+
+        public int Quantity { get; set; }
+        public string Status { get; set; } = "OK";
+    }
+}
